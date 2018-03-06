04d456ec7a137c9dd14d23b036f4ad96dceba2e2
Jan Viljanen <527069+javian@users.noreply.github.com>
2018-03-06 02:53:53 +0100

    audit fix and updates

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -1,6 +1,6 @@
 class WpCli < Formula
-  desc "Dependency Manager for PHP"
-  homepage "https://getcomposer.org"
+  desc "Command line interface for WordPress"
+  homepage "https://wp-cli.org"
   url "https://github.com/wp-cli/wp-cli/releases/download/v1.5.0/wp-cli-1.5.0.phar"
   sha256 "f615d57957e66a09f57acc844a1fc5402e9fa581dcb387bbe1affc4d155baf9d"
   revision 1
@@ -9,7 +9,7 @@ class WpCli < Formula
 
   def install
     libexec.install "wp-cli-#{version}.phar"
-    
+
     # We want to prevent the self update functionality in the script and
     # Tools like Ansible, expect the wrapper to be a PHP script.
     # https://github.com/Homebrew/homebrew-php/issues/3590
