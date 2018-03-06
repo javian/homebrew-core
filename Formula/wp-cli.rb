b20c0441ec6ab695cbf8222a6f88500a4078a4fc
Jan Viljanen <527069+javian@users.noreply.github.com>
2018-03-06 02:54:38 +0100

    audit fix

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -1,5 +1,5 @@
 class WpCli < Formula
-  desc "Command line interface for WordPress"
+  desc "Command-line interface for WordPress"
   homepage "https://wp-cli.org"
   url "https://github.com/wp-cli/wp-cli/releases/download/v1.5.0/wp-cli-1.5.0.phar"
   sha256 "f615d57957e66a09f57acc844a1fc5402e9fa581dcb387bbe1affc4d155baf9d"
