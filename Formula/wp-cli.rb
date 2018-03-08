81ecbf4a0c36fc301e12521cd96bf53ce1501acb
Jan Viljanen <527069+javian@users.noreply.github.com>
2018-03-08 14:13:54 +0100

    remove revision

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -3,7 +3,6 @@ class WpCli < Formula
   homepage "https://wp-cli.org"
   url "https://github.com/wp-cli/wp-cli/releases/download/v1.5.0/wp-cli-1.5.0.phar"
   sha256 "f615d57957e66a09f57acc844a1fc5402e9fa581dcb387bbe1affc4d155baf9d"
-  revision 1
 
   bottle :unneeded
 
